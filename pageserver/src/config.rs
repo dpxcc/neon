9c7295bfe1c1e00f19bd976eedba5700c53a96ef
Tristan Partin <tristan@neon.tech>
2024-10-30 18:38:25 -0500

    Switch compute-related locales to C.UTF-8 by default
    The C locale is not very useful. C.UTF-8 will come as less of a surprise
    to customers as they deal with UTF-8 data more regularly.
    
    Signed-off-by: Tristan Partin <tristan@neon.tech>

--- pageserver/src/config.rs
+++ pageserver/src/config.rs
@@ -69,6 +69,7 @@ pub struct PageServerConf {
     pub wal_redo_timeout: Duration,
 
     pub superuser: String,
+    pub locale: String,
 
     pub page_cache_size: usize,
     pub max_file_descriptors: usize,
@@ -298,6 +299,7 @@ impl PageServerConf {
             wait_lsn_timeout,
             wal_redo_timeout,
             superuser,
+            locale,
             page_cache_size,
             max_file_descriptors,
             pg_distrib_dir,
@@ -344,6 +346,7 @@ impl PageServerConf {
             wait_lsn_timeout,
             wal_redo_timeout,
             superuser,
+            locale,
             page_cache_size,
             max_file_descriptors,
             http_auth_type,
